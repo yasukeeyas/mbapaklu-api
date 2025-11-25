6aa23bff4e4954dbb21aa09443c11227bad5d3a0
yasukeeyas <iksanpauu02@gmail.com>
2025-11-25 20:52:29 +0700

    Add files via upload

---
+++ config.js
@@ -0,0 +1,9 @@
+module.exports = {
+  API_KEYS: ['adminv2', 'totoy', 'ALOK', 'putz', 'peler', 'Lz666Era', 'BALMOND'],
+  SMTP_HOST: 'smtp.gmail.com',
+  SMTP_PORT: 465,
+  SMTP_SECURE: true,
+  DEFAULT_RECIPIENT: 'smb@support.whatsapp.com',
+  LOG_FILE: __dirname + '/email_logs.txt',
+  DATA_FILE: __dirname + '/dataimel.txt',
+}
